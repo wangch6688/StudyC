ac0e80f87ba811d65048a9811201fd703954003f
Kevin <wangch6688@hotmail.com>
2020-04-02 16:30:03 +0800

    【ADD】String学习

--- CBaseStudy/CBaseStudy/ArrayStudy.cs
+++ CBaseStudy/CBaseStudy/ArrayStudy.cs
@@ -1,7 +1,114 @@
 ﻿using System;
+using System.Collections.Generic
 namespace CBaseStudy
 {
-    public interface ArrayStudy
+    public class ArrayStudyClass
     {
+        public void ArrayLearn()
+        {
+            int[] balance = new int[10];
+            balance[0] = 1000;
+
+            double[] balance2 = { 1.0, 2.0, 3.0, 4.0 };
+            int[] marks = new int[5] { 1, 2, 3, 4, 5 };
+
+            int i, j;
+
+            for (i = 0; i < 10; i++)
+            {
+                balance[i] = i + 1000;
+            }
+
+            for (j = 0; j < 10; j++)
+            {
+                Console.WriteLine("Element[{0}] = {1}", j, balance[j]);
+            }
+
+            //针对已知类型的数组，可以用foreach
+            foreach (int x in balance)
+            {
+                Console.WriteLine("element:{0}", x);
+            }
+
+            Console.ReadKey();
+        }
+
+
+
+        //在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
+        //每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+        public bool Find(int target, int[][] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length, j++)
+                {
+                    if(array[i][j] == target)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        /*
+        在一个长度为n的数组里的所有数字都在0到n-1的范围内。
+        数组中某些数字是重复的，但不知道有几个数字是重复的。
+        也不知道每个数字重复几次。请找出数组中任意一个重复的数字。
+        例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
+         */
+        public bool duplicate(int[] numbers, int[] duplication)
+        {
+            if (numbers.Length == 0) return false;
+            int[] result = new int[numbers.Length];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = -1;
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                foreach(int res in result)
+                {
+                    if (res == numbers[i])
+                    {
+                        duplication[0] = res;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        /*给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],
+         * 其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。
+         * 不能使用除法。（注意：规定B[0] = A[1] * A[2] * ... * A[n-1]，
+         * B[n-1] = A[0] * A[1] * ... * A[n-2];）*/
+
+        public int[] multiply(int[] A)
+        {
+            var B = new int[A.Length];
+            var left = new int[A.Length];
+            var right = new int[A.Length];
+
+            left[0] = 1;
+            right[A.Length - 1] = 1;
+
+            for (var i = 1; i < A.Length; i++)
+            {
+                left[i] = left[i - 1] * A[i - 1];
+            }
+
+            for (var i = A.Length - 2; i >= 0; i--)
+            {
+                right[i] = right[i + 1] * A[i + 1];
+            }
+
+            for (var i = 0; i < A.Length; i++)
+            {
+                B[i] = left[i] * right[i];
+            }
+
+            return B;
+        }
     }
 }
---
+++ CBaseStudy/CBaseStudy/StringStudy.cs
@@ -0,0 +1,84 @@
+﻿using System;
+namespace CBaseStudy
+{
+    
+    public class StringSty
+    {
+        public void StringBase()
+        {
+            string aStr, bStr;
+            aStr = "Hello  World";
+            bStr = "I)* $lo1314ve$% 2020 !@diNG()talk";
+
+            //字符串连接
+            string fullName = aStr + bStr;
+            //使用string构造函数  
+            char[] letters = { 'H', 'E', 'L', 'L', 'I' };
+            string meeting = new string(letters);
+            Console.WriteLine("meeting:{0}", meeting);
+
+            //方法返回字符串
+            string[] strArr = { "Hello", "From", "Tutorials", "Point" };
+            string message = String.Join(" ", strArr);
+
+        }
+
+        /*请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+         * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。*/
+        public string replaceSpace(string str)
+        {
+            if (!str.Contains(" ")) return str;
+            str = str.Replace(" ", "%20");
+            return str;
+        }
+
+        /*
+            "I)* $lo1314ve$% 2020 !@diNG()talk"，过滤字符串特殊字符，
+            然后每个单词首字符大写， 输出I Love Dingtalk
+         */
+
+        public string showStr(string str)
+        {
+            return " ";
+        }
+
+        public bool isNumeric(string str)
+        {
+            // 标记符号、小数点、e是否出现过
+            bool sign, hasDecimal, hasE;
+            sign = false;
+            hasDecimal = false;
+            hasE = false;
+      
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].Equals("e") || str[i].Equals("E"))
+                {
+                    if (i == str.Length - 1) return false; // e后面一定要接数字
+                    if (hasE) return false;  // 不能同时存在两个e
+                    hasE = true;
+                }
+                else if (str[i].Equals("+") || str[i].Equals("-"))
+                {
+                    // 第二次出现+-符号，则必须紧接在e之后
+                    if (sign && !str[i - 1].Equals("e") && !str[i - 1].Equals("E")) return false;
+                    // 第一次出现+-符号，且不是在字符串开头，则也必须紧接在e之后
+                    if (!sign && i > 0 && !str[i - 1].Equals("e") && !str[i - 1].Equals("EE")) return false;
+                    sign = true;
+                }
+                else if (str[i].Equals("."))
+                {
+                    // e后面不能接小数点，小数点不能出现两次
+                    if (hasE || hasDecimal) return false;
+                    hasDecimal = true;
+                }
+                    return false;
+            }
+            return true;
+        }
+    }
+
+
+
+
+}
